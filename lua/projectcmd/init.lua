93508f1374fbfcf661f97f31f74f3f0ea3d6d216
Arnold Chand <creativenull@outlook.com>
2020-12-11 23:04:31 -0400

    feat: add sourcing option, completing the intended project goal

--- lua/projectcmd/init.lua
+++ lua/projectcmd/init.lua
@@ -16,12 +16,12 @@ local function has_key(key)
 end
 
 -- Checks if the file exists and match the key
-local function is_key_match(key, filepath)
-    if utils.file_exists(filepath) == false then
+local function is_key_match(opts)
+    if utils.file_exists(opts.filepath) == false then
         return false
     end
 
-    local fp = io.open(filepath)
+    local fp = io.open(opts.filepath)
     local first_line = io.input(fp):read()
     fp.close()
 
@@ -29,8 +29,14 @@ local function is_key_match(key, filepath)
         return false
     end
 
-    local filekey = string.sub(first_line, 3)
-    return filekey == key
+    local filekey = ''
+    if opts.type == 'vim' then
+        filekey = string.sub(first_line, 3)
+    elseif opts.type == 'lua' then
+        filekey = string.sub(first_line, 2)
+    end
+
+    return filekey == opts.key
 end
 
 -- Validate user options
@@ -56,18 +62,24 @@ local function validate_opts(opts)
     return opts
 end
 
+local function source_file(opts)
+    if opts.type == 'vim' then
+        vim.cmd('source ' .. opts.filepath)
+    elseif opts.type == 'lua' then
+        vim.cmd('luafile ' .. opts.filepath)
+    end
+end
+
 -- Loads the settings.vim into runtime
 local function setup(opts)
     local _opts = validate_opts(opts)
 
     if has_key(_opts.key) then
-        if is_key_match(opts.key, opts.filepath) then
-            print(PROJECTCMD .. ' Found!')
-        else
-            print(PROJECTCMD .. ' Key Mismatch or no key provided!')
+        if is_key_match(opts) then
+            source_file(opts)
         end
     else
-        print(PROJECTCMD .. ' Not Found!')
+        print(PROJECTCMD .. ' Key is required!')
     end
 end
 
