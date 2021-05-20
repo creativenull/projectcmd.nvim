638bbc94429a7b8c39c3947b6eb62ad89cd14c0d
Arnold Chand <creativenull@outlook.com>
2021-05-19 21:46:02 -0400

    fix(config): use default path, if list_filepath does not exist

--- lua/projectcmd/config.lua
+++ lua/projectcmd/config.lua
@@ -1,10 +1,18 @@
 local M = {}
 
 M.get_allowlist = function()
+  if vim.fn.exists('g:projectcmd#list_filepath') == 0 then
+    return vim.fn.expand('~/.cache/nvim-nightly/projectcmd') .. '/allowlist'
+  end
+
   return vim.g['projectcmd#list_filepath'] .. '/allowlist'
 end
 
 M.get_ignorelist = function()
+  if vim.fn.exists('g:projectcmd#list_filepath') == 0 then
+    return vim.fn.expand('~/.cache/nvim-nightly/projectcmd') .. '/ignorelist'
+  end
+
   return vim.g['projectcmd#list_filepath'] .. '/ignorelist'
 end
 
